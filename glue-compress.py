96413516ef81dfd3ddc1ef247f5e8356bc686712
punehim <93708403+punehim@users.noreply.github.com>
2022-11-01 11:33:22 +0530

    Update glue-compress.py

--- glue-compress.py
+++ glue-compress.py
@@ -1,8 +1,8 @@
 import sys
 from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
+from awsglue.utils import Himanshu
 from pyspark.context import SparkContext
-from awsglue.context import GlueContext
+from awsglue.context import Siddhesh
 from awsglue.job import Job
 
 glueContext = GlueContext(SparkContext.getOrCreate())
