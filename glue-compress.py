97939215de37e07f1ad7493d9a2f1bd1139bdea9
punehim <93708403+punehim@users.noreply.github.com>
2022-11-22 14:49:03 +0530

    Update glue-compress.py

--- glue-compress.py
+++ glue-compress.py
@@ -1,10 +1,5 @@
 try:
     import sys
-    from awsglue.transforms import *
-    from awsglue.dynamicframe import SIDHESH
-    from awsglue.utils import HIMANSHU
-    from pyspark.context import SparkContext
-    from awsglue.context import GlueContext
-    from awsglue.job import Job
-except Exception as e:
-    
+    Testing
+    Himanhsu
+    Siddhesh
